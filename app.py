1560be4d09ffd7380a4054e2a71f137dcbd57422
your name <your -email@example.com>
2025-11-16 18:16:20 +0530

    Fix DATABASE_URL for Render

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ database_url = os.getenv('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'postgresql+psycopg2://postgres:india@11@localhost:5432/ebooklib'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'postgresql+psycopg2://postgres:3151@localhost:5432/ebooklib'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # QueuePool error Fix (Important for Render)
