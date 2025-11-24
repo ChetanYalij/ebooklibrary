3316763a000571be5f237d7b2f495294d1e87220
your name <your -email@example.com>
2025-11-24 12:19:53 +0530

    Fix file name

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ database_url = os.getenv('DATABASE_URL')
 if database_url and database_url.startswith('postgres://'):
     database_url = database_url.replace('postgres://', 'postgresql+psycopg2://', 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///ebooklib.db'
-}
 db.init_app(app)
 
 # ========= CLOUDINARY =========
